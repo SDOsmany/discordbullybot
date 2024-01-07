708dc00528f04ae029bf49b6979072af78cfdfcc
osmany <osmany.pujolcs@gmail.com>
2024-01-06 21:52:34 -0500

    added the ability to roast other people or yourself

--- main.py
+++ main.py
@@ -5,9 +5,19 @@ from discord.ext import commands
 import discord
 from dotenv import load_dotenv
 
+import pandas as pd
+
+# load the environment variables
 load_dotenv()
 TOKEN = os.getenv('bot_key')
 CHANNEL_ID = os.getenv('channel_id')
+
+# read the list of roasts
+path = "roast_list.csv"
+abs_path = os.path.abspath(path)
+roasting_list = pd.read_csv(abs_path, header=None)
+
+# initialize the bot
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 
@@ -23,11 +33,14 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    # Check if the message is "!" and respond
+    if message.mentions:
+        for mention in message.mentions:
+            if "!bully" in message.content:
+                response = str(mention) + " " + roasting_list.sample(n=1).iloc[0,0]
+                await message.channel.send(response)
+    # Check if the message is "!bully" and respond
     if message.content == "!bully":
-        await message.channel.send("You suck!")
-
-    # This is necessary for other commands to work
-    await bot.process_commands(message)
+        response = message.author.mention + " " + roasting_list.sample(n=1).iloc[0,0]
+        await message.channel.send(response)
 
 bot.run(TOKEN)
